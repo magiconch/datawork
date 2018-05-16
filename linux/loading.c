626954fa4fec5cf6d4704e3378d16f6748fad4e2
magiconch <magiconch@outlook.com>
2018-05-16 18:50:41 +0800

    xinde

--- linux/loading.c
+++ linux/loading.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 int main(){
+  char buf[50]={'#'};
   int i = 0;
   for (i=0;i< 50;++i){
-    printf("#");
+    buf[i] = '\0';
+    printf("[%d] %s",i*2,buf);
     fflush(stdout);
     usleep(30000);
+    buf[i]= '#';
   }
   printf("\n");
   return 0;
