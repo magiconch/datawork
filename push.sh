2d61cd3556f5dcefaa9304b65194c5ebbaf72d1f
magiconch <magiconch@outlook.com>
2018-06-05 17:41:31 +0800

    增加了线程任务

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ git add . || git add --all
 echo 输入注释
 read mycommit
 git commit -m $mycommit
-echo 是否需要提交github(y/n)
+echo 是否需要提交github"(y/n)"
 read stat
 
 ['X'$stat=='X'"y"] && git push origin master || sh fuck.sh
