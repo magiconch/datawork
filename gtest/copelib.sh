63dc76c2f5b9a0914f179d40d6a9e94faf8e9c56
magiconch <magiconch@outlook.com>
2018-07-19 13:17:00 +0800

    添加了gtest

---
+++ gtest/copelib.sh
@@ -0,0 +1,3 @@
+cp -a ~/install/googletest-release-1.8.0/googletest/include/* ~/datawork/gtest/include/
+cp -a ~/install/googletest-release-1.8.0/build/googlemock/gtest/libgtest.a  ~/datawork/gtest/lib/
+cp -a ~/install/googletest-release-1.8.0/build/googlemock/gtest/libgtest_main.a  ~/datawork/gtest/lib/
